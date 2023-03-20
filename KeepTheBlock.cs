2dd0ab3321f3f901c86ae7883a1bf855626d17fd
Dexy <mugiwara68@gmail.com>
2023-03-20 20:02:21 +0100

    Increased version

---
+++ KeepTheBlock.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+
+namespace KeepTheBlock
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class KeepTheBlock : BaseUnityPlugin
+    {
+
+        private const string ModGUID = "Dexy-KeepTheBlock";
+        private const string ModName = "KeepTheBlock";
+        private const string ModVersion = "1.0.1";
+        internal static new ManualLogSource Logger { get; set; }
+
+        private void Awake()
+        {
+
+            // Get the BepInEx Logger //
+            KeepTheBlock.Logger = base.Logger;
+
+            // Log the Start of the Plugin //
+            Logger.LogInfo("Plugin KeepTheBlock Added!");
+
+            Utils.Hooks.AddHooks(typeof(MatchManager), nameof(MatchManager.ConsumeAuraCurse), AllowBlockToStay);
+
+        }
+
+        private static void AllowBlockToStay(Action<MatchManager, string, Character, string> orig, MatchManager self, string whenToConsume, Character character, string auraToConsume = "")
+        {
+
+            // Set the Block to not be removed at the end of the Round //
+            foreach (Aura aura in character.AuraList)
+            {
+                if (aura.acData != null && aura.acData.id == "block")
+                    aura.acData.NoRemoveBlockAtTurnEnd = true;
+            }
+
+            orig(self, whenToConsume, character, auraToConsume);
+        }
+    }
+}
---
+++ Utils/Hooks.cs
@@ -0,0 +1,47 @@
+﻿using MonoMod.RuntimeDetour.HookGen;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace KeepTheBlock.Utils
+{
+    internal class Hooks
+    {
+
+        public static void AddHooks(Type classType, string methodName, Delegate calledMethod)
+        {
+            AddHooks(classType, methodName, null, calledMethod);
+        }
+
+        public static void AddHooks(Type classType, string methodName, Type[] methodType, Delegate calledMethod)
+        {
+
+            // Check all values //
+            if (classType == null || methodName == null || calledMethod == null)
+            {
+                UnityEngine.Debug.LogWarning("KeepTheBlock -> Failed to create hook, argument null: " + classType.Name + "." + methodName + " -> " + calledMethod.Method.Name);
+                return;
+            }
+
+            // Get the Method //
+            MethodInfo method = null;
+            if (methodType != null)
+                method = classType.GetMethod(methodName, (BindingFlags)(-1), null, methodType, null);
+            else
+                method = classType.GetMethod(methodName, (BindingFlags)(-1));
+
+
+            // Check the Method //
+            if (method == null)
+            {
+                UnityEngine.Debug.LogWarning("KeepTheBlock -> Failed to create hook, method null : " + classType.Name + "." + methodName + " -> " + calledMethod.Method.Name);
+                return;
+            }
+
+            HookEndpointManager.Add(method, calledMethod);
+            UnityEngine.Debug.Log("KeepTheBlock -> Hook Added for " + classType.Name + "." + methodName + " -> " + calledMethod.Method.Name);
+
+        }
+
+    }
+}
